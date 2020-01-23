4ef265c53a6f23a38b5dbd795df722d47ddd9fb8
pknaus3 <peterknaus@knausweb.com>
2020-01-23 17:01:46 +0100

    Installed OctoberCMS
    None

---
+++ storage/framework/classes.php
@@ -0,0 +1,12 @@
+<?php return array (
+  'october\\demo\\Plugin' => 'plugins\\october\\demo\\Plugin.php',
+  'backend\\Controllers\\index' => 'modules\\backend\\controllers\\index.php',
+  'backend\\Controllers\\auth' => 'modules\\backend\\controllers\\auth.php',
+  'backend\\Controllers\\users' => 'modules\\backend\\controllers\\users.php',
+  'system\\Controllers\\updates' => 'modules\\system\\controllers\\updates.php',
+  'rainlab\\user\\Plugin' => 'plugins\\rainlab\\user\\Plugin.php',
+  'RainLab\\User\\Models\\User' => 'plugins\\rainlab\\user\\models\\User.php',
+  'RainLab\\User\\Models\\Settings' => 'plugins\\rainlab\\user\\models\\Settings.php',
+  'RainLab\\User\\Models\\UserGroup' => 'plugins\\rainlab\\user\\models\\UserGroup.php',
+  'RainLab\\User\\Classes\\UserRedirector' => 'plugins\\rainlab\\user\\classes\\UserRedirector.php',
+);
