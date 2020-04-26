92ee6ddbe31b96131f5b8bd31e29a1c9ff1b5a48
Peter Knaus <pknaus3@gmail.com>
2020-04-26 07:05:05 +0200

    created components
    components for frontend adding course complet but isn't working

---
+++ plugins/academy/course/components/CourseFE.php
@@ -0,0 +1,19 @@
+<?php namespace Academy\Course\Components;
+
+use Cms\Classes\ComponentBase;
+
+class CourseFE extends ComponentBase
+{
+    public function componentDetails()
+    {
+        return [
+            'name'        => 'courseFE Component',
+            'description' => 'No description provided yet...'
+        ];
+    }
+
+    public function defineProperties()
+    {
+        return [];
+    }
+}
---
+++ plugins/academy/course/components/EditorFE.php
@@ -0,0 +1,19 @@
+<?php namespace Academy\Course\Components;
+
+use Cms\Classes\ComponentBase;
+
+class EditorFE extends ComponentBase
+{
+    public function componentDetails()
+    {
+        return [
+            'name'        => 'editorFE Component',
+            'description' => 'No description provided yet...'
+        ];
+    }
+
+    public function defineProperties()
+    {
+        return [];
+    }
+}
