ce2be7a7efb70c75c9a7b2e512dc70de124b2b77
Peter Knaus <pknaus3@gmail.com>
2020-04-28 18:31:29 +0200

    Update routes.php

--- plugins/academy/course/updates/routes.php
+++ plugins/academy/course/updates/routes.php
@@ -4,9 +4,11 @@
 use October\Rain\Auth\Models\User;
 
 Route::post('chcek', function ($req) {
+    $data = $req->input();
+
     $check = new CheckBox();
     $check->user = User::getAuth()->id;
-    $check->step_id = $req['step_id'];
-    $check->is_checked = $req['is_checked'];
+    $check->step_id = $data['step_id'];
+    $check->is_checked = $data['is_checked'];
     $check->save();
 });
