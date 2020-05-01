6bb20390b53223c8b6d3b3bb90d3e63174826476
Peter Knaus <pknaus3@gmail.com>
2020-05-02 00:00:39 +0200

    Update Excel.php

--- plugins/academy/course/components/Excel.php
+++ plugins/academy/course/components/Excel.php
@@ -2,7 +2,9 @@
 
 use Academy\Course\Models\Step;
 use Cms\Classes\ComponentBase;
+use http\Env\Request;
 use Illuminate\Support\Facades\Input;
+
 
 class Excel extends ComponentBase
 {
@@ -22,13 +24,22 @@ class Excel extends ComponentBase
     public function onSubmit() {
         $file = Input::file('file');
         $fileContent = file_get_contents($file->getRealPath());
-        var_dump($fileContent);
+        //var_dump($fileContent);
         self::_inserData($fileContent);
+
+        $json = '﻿{"courseId":1,"test":5}';
+
+        dump(json_decode($json));
+
+        $json1 = '{"Peter":65,"Harry":80,"John":78,"Clark":90}';
+
+        //var_dump(json_decode($json1));
+
     }
     static function _inserData($fileContent){
         $step = json_decode($fileContent);
-        var_dump($step);
 
+/*'{"courseId":1,"test":5}'
         $req = new Step();
         $req->course_id = $fileContent['course_id'];
         $req->step_name = $fileContent['step_name'];
@@ -39,7 +50,7 @@ class Excel extends ComponentBase
         $req->step_position = $fileContent['step_position'];
         $req->homework = $fileContent['homework'];
         $req->save();
-
+*/
     }
 
 }
