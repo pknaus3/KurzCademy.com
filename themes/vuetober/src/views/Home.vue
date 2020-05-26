73a050c6d412a20bf42acc5ecdc37d05ef7fec26
bt7s7k7 <bt7s7k7@hotmail.sk>
2020-05-26 17:11:38 +0200

    Gutted example code

--- themes/vuetober/src/views/Home.vue
+++ themes/vuetober/src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div>
+        Hello world
+    </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<script lang="ts">
+import Vue from 'vue'
+import Component from "vue-class-component"
+import * as vueProp from "vue-property-decorator"
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+@Component
+export default class Home extends Vue {
+   
 }
 </script>
