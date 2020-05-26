dffe97248b89f8cd6e0d22005abadc91d5837605
Peter Knaus <pknaus3@gmail.com>
2020-05-26 21:42:31 +0200

    Update routes.php

--- plugins/academy/course/routes.php
+++ plugins/academy/course/routes.php
@@ -1,6 +1,9 @@
 <?php
 
 use October\Rain\Auth\Models\User;
+use Academy\Course\Models\Course;
+use Academy\Course\Models\Step;
+
 
 Route::post('/check', function ($req) {
     $data = $req->input();
@@ -10,5 +13,14 @@ Route::post('/check', function ($req) {
     $check->step_id = $data['step_id'];
     $check->is_checked = $data['is_checked'];
     $check->save();
+});
+
+Route::get('api/courses', function () {
+    $courses = Course::All();
+    return $courses;
+});
+Route::get('api/steps', function () {
+    $steps = Step::All();
+    return $steps;
 });
 
