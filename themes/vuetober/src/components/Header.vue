8304b168af484a301f368ff7e1045a36090e2efc
bt7s7k7 <bt7s7k7@hotmail.sk>
2020-05-28 16:43:21 +0200

    Header user menu fixed

--- themes/vuetober/src/components/Header.vue
+++ themes/vuetober/src/components/Header.vue
@@ -4,40 +4,50 @@
 			<b-img src="@/assets/img/logo/KurzCademyLogoBezTextu.png" height="50px"></b-img>
 		</b-navbar-brand>
 		<div class="flex-fill"></div>
-		<b-btn v-if="userData.user" variant="outline-dark" class="d-flex flex-row align-items-center user-button" style="border: none">
+		<b-btn
+			v-if="userData.user"
+			variant="outline-dark"
+			class="d-flex flex-row align-items-center user-button"
+		>
 			<b-avatar :src="userData.user.avatar.path"></b-avatar>
-            <div class="font-weight-bold ml-2">{{ userData.user.name }}</div>
-            <div class="user-menu flex-column">
-                <b-btn variant="light" to="/account">Môj účet</b-btn>
-                <b-btn variant="light" @click="logout()">Odhlásiť</b-btn>
-            </div>
+			<div class="ml-2">{{ userData.user.name }}</div>
+			<div class="d-flex user-menu flex-column">
+				<b-btn variant="light" to="/account">Môj účet</b-btn>
+				<b-btn variant="light" @click="logout()">Odhlásiť</b-btn>
+			</div>
 		</b-btn>
 	</b-navbar>
 </template>
 
 <style scoped>
-    .user-button {
-        position: relative;
-    }
+	.user-button {
+		position: relative;
+		border: none;
+		font-weight: bold;
+        z-index: 2;
+	}
 
-    .user-menu {
-        display: none;
-        opacity: 0;
-        position: absolute;
-        top: 100%;
-        left: 0;
-        background-color: white;
-        width: 100%;
-    }
+	.user-menu {
+        visibility: collapse;
+		opacity: 0;
+		position: absolute;
+		top: calc(100% - 4px);
+		left: 0;
+		background-color: white;
+		width: 100%;
+        border-radius: 4px;
+        border-top-left-radius: 0;
+        border-top-right-radius: 0;
+	}
 
-    .user-menu > * {
-        text-align: left;
-    }
+	.user-menu > * {
+		text-align: left;
+	}
 
-    .user-button:hover > .user-menu {
-        display: flex;
-        opacity: 1;
-    }
+	.user-button:hover > .user-menu {
+        visibility: visible;
+		opacity: 1;
+	}
 </style>
 
 <script lang="ts">
@@ -48,10 +58,10 @@
 
 	@Component
 	export default class Header extends Vue {
-        userData = userData
-        
-        logout() {
-            logout()
-        }
+		userData = userData
+
+		logout() {
+			logout()
+		}
 	}
 </script>
