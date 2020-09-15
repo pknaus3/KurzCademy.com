b7fdd6778b3d33d92782686f7f5c6304cd44b5a9
Peter Knaus <pknaus3@gmail.com>
2020-09-15 14:59:42 +0200

    Favorite Course - Refactor BE

---
+++ plugins/pknaus3/user/Plugin.php
@@ -0,0 +1,96 @@
+<?php namespace Pknaus3\User;
+
+use Backend;
+use System\Classes\PluginBase;
+
+/**
+ * user Plugin Information File
+ */
+class Plugin extends PluginBase
+{
+    /**
+     * Returns information about this plugin.
+     *
+     * @return array
+     */
+    public function pluginDetails()
+    {
+        return [
+            'name'        => 'user',
+            'description' => 'No description provided yet...',
+            'author'      => 'pknaus3',
+            'icon'        => 'icon-leaf'
+        ];
+    }
+
+    /**
+     * Register method, called when the plugin is first registered.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Boot method, called right before the request route.
+     *
+     * @return array
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Registers any front-end components implemented in this plugin.
+     *
+     * @return array
+     */
+    public function registerComponents()
+    {
+        return []; // Remove this line to activate
+
+        return [
+            'Pknaus3\User\Components\MyComponent' => 'myComponent',
+        ];
+    }
+
+    /**
+     * Registers any back-end permissions used by this plugin.
+     *
+     * @return array
+     */
+    public function registerPermissions()
+    {
+        return []; // Remove this line to activate
+
+        return [
+            'pknaus3.user.some_permission' => [
+                'tab' => 'user',
+                'label' => 'Some permission'
+            ],
+        ];
+    }
+
+    /**
+     * Registers back-end navigation items for this plugin.
+     *
+     * @return array
+     */
+    public function registerNavigation()
+    {
+        return []; // Remove this line to activate
+
+        return [
+            'user' => [
+                'label'       => 'user',
+                'url'         => Backend::url('pknaus3/user/mycontroller'),
+                'icon'        => 'icon-leaf',
+                'permissions' => ['pknaus3.user.*'],
+                'order'       => 500,
+            ],
+        ];
+    }
+}
